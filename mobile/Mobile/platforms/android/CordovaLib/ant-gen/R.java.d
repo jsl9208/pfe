024f6110dbf19fe333d6c7968387f8a7a4efe92e
Albert Jiang <jiangshilong9208@gmail.com>
2015-04-22 16:21:19 +0800

    add in Phonegap app source code

---
+++ mobile/Mobile/platforms/android/CordovaLib/ant-gen/R.java.d
@@ -0,0 +1,1 @@
+ : /Users/mac/Projects/mobile_phone/Mobile/platforms/android/CordovaLib/ant-build/AndroidManifest.xml \
---
+++ mobile/Mobile/platforms/android/ant-build/HelloCordova-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/Users/mac/Projects/mobile_phone/Mobile/platforms/android/ant-build/HelloCordova-debug-unaligned.apk : \
+/Users/mac/Projects/mobile_phone/Mobile/platforms/android/ant-build/HelloCordova.ap_ \
+/Users/mac/Projects/mobile_phone/Mobile/platforms/android/ant-build/classes.dex \
+/Users/mac/Projects/mobile_phone/Mobile/platforms/android/CordovaLib/ant-build/classes.jar \
