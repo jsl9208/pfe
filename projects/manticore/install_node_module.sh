f56194e19cd0d59107d059a8e1583f8dd5a07f22
Thibault F <t.fordeveaux@gmail.com>
2015-05-19 10:32:00 +0800

    Script install npm

---
+++ projects/manticore/install_node_module.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+npm install async
+npm install body-parser
+npm install bunyan
+npm install express
+npm install jade
+npm install mdns
+npm install osc
+npm install performance-now
+npm install request
+npm install underscore
+npm install uuid
+npm install websocket
+npm install ws
+npm install zmq
