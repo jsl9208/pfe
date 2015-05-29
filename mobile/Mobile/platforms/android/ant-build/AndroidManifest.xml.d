46ce6c225af6f002e723a4b8761a692b69cc19d5
Thibault F <t.fordeveaux@gmail.com>
2015-05-29 09:55:52 +0800

    Mobile app added to the branch

---
+++ mobile/Mobile/platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/mac/Projects/mobile_phone/Mobile/platforms/android/ant-build/AndroidManifest.xml : \
+/Users/mac/Projects/mobile_phone/Mobile/platforms/android/AndroidManifest.xml \
---
+++ mobile/Mobile/platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,12 @@
+/Users/mac/Projects/mobile_phone/Mobile/platforms/android/ant-build/classes.dex : \
+/Users/mac/Projects/mobile_phone/Mobile/platforms/android/ant-build/classes/io/cordova/hellocordova/BuildConfig.class \
+/Users/mac/Projects/mobile_phone/Mobile/platforms/android/ant-build/classes/io/cordova/hellocordova/HelloCordova.class \
+/Users/mac/Projects/mobile_phone/Mobile/platforms/android/ant-build/classes/io/cordova/hellocordova/R$attr.class \
+/Users/mac/Projects/mobile_phone/Mobile/platforms/android/ant-build/classes/io/cordova/hellocordova/R$drawable.class \
+/Users/mac/Projects/mobile_phone/Mobile/platforms/android/ant-build/classes/io/cordova/hellocordova/R$string.class \
+/Users/mac/Projects/mobile_phone/Mobile/platforms/android/ant-build/classes/io/cordova/hellocordova/R$xml.class \
+/Users/mac/Projects/mobile_phone/Mobile/platforms/android/ant-build/classes/io/cordova/hellocordova/R.class \
+/Users/mac/Projects/mobile_phone/Mobile/platforms/android/ant-build/classes/org/apache/cordova/device/Device.class \
+/Users/mac/Projects/mobile_phone/Mobile/platforms/android/ant-build/classes/org/apache/cordova/devicemotion/AccelListener$1.class \
+/Users/mac/Projects/mobile_phone/Mobile/platforms/android/ant-build/classes/org/apache/cordova/devicemotion/AccelListener.class \
+/Users/mac/Projects/mobile_phone/Mobile/platforms/android/ant-build/dexedLibs/classes-87b9d6acbd57b739f1964d0b0b15bf15.jar \
