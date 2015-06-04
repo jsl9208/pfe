84cf55d93d71b7971fffe6ccf72f16933ad1f71c
Ghita Tazi <ghita.tazi@insa-lyon.fr>
2015-06-04 12:59:35 +0800

    changes in timeTag = 60 of index.js and logs put in multiple levels

--- mobile/Mobile/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++ mobile/Mobile/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-/Users/thibaultfordeveaux/pfe/mobile/Mobile/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/Users/thibaultfordeveaux/pfe/mobile/Mobile/platforms/android/CordovaLib/AndroidManifest.xml \
+/Users/ghitatazi/GitHub/pfe_Test/mobile/Mobile/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/Users/ghitatazi/GitHub/pfe_Test/mobile/Mobile/platforms/android/CordovaLib/AndroidManifest.xml \
--- mobile/Mobile/platforms/android/ant-build/AndroidManifest.xml.d
+++ mobile/Mobile/platforms/android/ant-build/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-/Users/thibaultfordeveaux/pfe/mobile/Mobile/platforms/android/ant-build/AndroidManifest.xml : \
-/Users/thibaultfordeveaux/pfe/mobile/Mobile/platforms/android/AndroidManifest.xml \
+/Users/ghitatazi/GitHub/pfe_Test/mobile/Mobile/platforms/android/ant-build/AndroidManifest.xml : \
+/Users/ghitatazi/GitHub/pfe_Test/mobile/Mobile/platforms/android/AndroidManifest.xml \
